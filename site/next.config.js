1d5d7e4492d9f6244e531e069d0cd23549f834bc
jeff-bracewell <jeff.bracewell@gmail.com>
2025-03-24 12:40:29 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Jeff\'s Registry',
+    description: 'The official store for Jeff supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
